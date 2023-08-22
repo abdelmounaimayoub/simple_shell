9a1cf30cce7463469058fabf605945cc633565d3
ilyass <ilyass.daaif@gmail.com>
2023-08-22 10:38:41 -0700

    a

--- getline.c
+++ getline.c
@@ -72,7 +72,7 @@ ssize_t get_input(input_t *info)
 				break;
 			j++;
 		}
-		
+
 		i = j + 1; /* increment past nulled ';'' */
 		if (i >= len) /* reached end of buffer? */
 		{
@@ -133,18 +133,18 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	r = read_buf(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
-	
+
 	c = _strchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
 	if (!new_p) /* MALLOC FAILURE! */
 		return (p ? free(p), -1 : -1)
-	
+
 	if (s)
 		_strncat(new_p, buf + 1, k - i);
 	else
 		_strncpy(new_p, buf + i, k - i + 1);
-	
+
 	s += k - i;
 	i = k;
 	p = new_p;
