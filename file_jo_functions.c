2993e928b680d85c25bec029bb145fc5b7169033
ilyass <ilyass.daaif@gmail.com>
2023-08-22 10:34:03 -0700

    a

--- file_jo_functions.c
+++ file_jo_functions.c
@@ -9,11 +9,9 @@
 
 char *get_history_file(info_t *info)
 {
-
 char *buf, *dir;
 dir = _getenv(info, "HOME=");
 if (!dir)
-<<<<<<< HEAD
 return (NULL);
 buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
 if (!but)
@@ -27,28 +25,12 @@ return (buf);
 
 /**
  * write_history - creates afile, or appends to an existing file
-=======
- return (NULL);
-buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
-if (!but)
- return (NULL);
-buf[0] = 0;
- _strcpy(buf, dir);
- _strcat(buf, "/");
- _strcat(buf, HIST_FILE);
-    return (buf);
-}
-
-/**
- * write_history - creates afile, or appends to an existing file 
->>>>>>> 24409b331523865a45d9f30b35dbf67c5761a6df
  * @info: the parameter struct
  *
  * Return: 1 on success, else -1
  */
 int write_history(info_t *info)
 {
-<<<<<<< HEAD
 size_t fd;
 char *filename = get_history_file(info);
 list_t *node = NULL;
@@ -68,27 +50,6 @@ _putfd('\n', fd);
 _putfd(BUF_FLUSH, fd);
 close(fd);
 return (1);
-=======
-    size_t fd;
-    char *filename = get_history_file(info);
-    list_t *node = NULL;
-
-    if (!filename)
-	return (-1);
-
-    fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-    free(filename);
-    if (fd == -1)
-        return (-1);
-    for (node = info->history; node; node = node->next)
-    {
-	_putsfd(node->str, fd);
-        _putfd('\n', fd);
-    }
-    _putfd(BUF_FLUSH, fd);
-    close(fd);
-    return (1);
->>>>>>> 24409b331523865a45d9f30b35dbf67c5761a6df
 }
 
 /**
@@ -99,7 +60,6 @@ return (1);
  */
 int read_history(info_t *info)
 {
-<<<<<<< HEAD
 int i, last = 0, linecount = 0;
 ssize_t fd, rdlen, fsize = 0;
 struct stat st;
@@ -136,44 +96,6 @@ while (info->histcount-- >= HIST_MAX)
 	delete_node_at_index(&(info->history), 0);
 renumber_history(info);
 return (info->histcount);
-=======
-    int i, last = 0, linecount = 0;
-    ssize_t fd, rdlen, fsize = 0;
-    struct stat st;    
-    char *buf = NULL, *filename = get_history_file(info);
-
-    if (!filename)
-	return (0);
-
-    fd = open(filename, o_rdonly);
-    free(filename);
-    if (fd == -1)
-	return (0);
-    if (!fstat(fd, &st))
-	fsize = st.st_size;
-    if (fsize < 2)
-	return (0);
-    rdlen = read(fd, buf, fsize);
-    buf[fsize] = 0;
-    if (rdlen <= 0)
-	return (free(buf), 0);
-    close(fd);
-    for (i = 0; i <fsize; i++)
-	if (buf[i] == '/n')
-	{
-	    buf[i] = 0;
-            build_history_list(info, buf + last, linecount++);
-            last = i + 1;
-        }
-    if (last != i)
-	build_history_list(info, buf + last, linecount++);
-    free(buf);
-    info->histcount = linecount; 
-    while (info->histcount-->= HIST_MAX)
-	delete_node_at_index(&(info->history), 0);
-    renumber_history(info);
-    return (info->histcount);
->>>>>>> 24409b331523865a45d9f30b35dbf67c5761a6df
 }
 
 /**
@@ -186,7 +108,6 @@ return (info->histcount);
  */
 int build_history_list(info_t *info, char *buf, int linecount)
 {
-<<<<<<< HEAD
 list_t *node = NULL;
 
 if (info->history)
@@ -200,28 +121,12 @@ return (0);
 
 /**
  * renumber_history - renumbers the history linked list after changes
-=======
-    list_t *node = NULL;
-
-    if (info->history)
-	node = info->history;
-    add_node_end(&node, buf , linecount);
-
-    if (!info->history)
-	info->history = node;
-    return (0);
-}
-
-/**
- * renumber_history - renumbers the history linked list after changes 
->>>>>>> 24409b331523865a45d9f30b35dbf67c5761a6df
  * @info: Structure containing potential arguments. Used to maintain
  *
  * Return: the new histcount
  */
 int renumber_history(info_t *info)
 {
-<<<<<<< HEAD
 list_t *node = info->history;
 int i = 0;
 
@@ -232,15 +137,3 @@ node = node->next;
 }
 return (info->histcount = i);
 }
-=======
-    list_t *node =info->history;
-    int i = 0;
-    
-    while (node)
-    {
-        node->num = i++;
-        node = node->next;
-    }
-    return (info->histcount = i);
-}    
->>>>>>> 24409b331523865a45d9f30b35dbf67c5761a6df
