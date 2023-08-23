c8010e3677e3d55f15bb6dd3a1280894ae6e9a89
ilyass <ilyass.daaif@gmail.com>
2023-08-23 12:45:55 -0700

    a

--- file_jo_functions.c
+++ file_jo_functions.c
@@ -10,11 +10,12 @@
 char *get_history_file(info_t *info)
 {
 char *buf, *dir;
+
 dir = _getenv(info, "HOME=");
 if (!dir)
 return (NULL);
 buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
-if (!but)
+if (!buf)
 return (NULL);
 buf[0] = 0;
 _strcpy(buf, dir);
@@ -24,14 +25,14 @@ return (buf);
 }
 
 /**
- * write_history - creates afile, or appends to an existing file
+ * write_history - creates a file, or appends to an existing file
  * @info: the parameter struct
  *
  * Return: 1 on success, else -1
  */
 int write_history(info_t *info)
 {
-size_t fd;
+ssize_t fd;
 char *filename = get_history_file(info);
 list_t *node = NULL;
 
@@ -44,7 +45,7 @@ if (fd == -1)
 return (-1);
 for (node = info->history; node; node = node->next)
 {
-	_putsfd(node->str, fd);
+_putsfd(node->str, fd);
 _putfd('\n', fd);
 }
 _putfd(BUF_FLUSH, fd);
@@ -68,7 +69,7 @@ char *buf = NULL, *filename = get_history_file(info);
 if (!filename)
 	return (0);
 
-fd = open(filename, o_rdonly);
+fd = open(filename, O_RDONLY);
 free(filename);
 if (fd == -1)
 	return (0);
@@ -76,18 +77,21 @@ if (!fstat(fd, &st))
 	fsize = st.st_size;
 if (fsize < 2)
 	return (0);
+buf = malloc(sizeof(char) * (fsize + 1));
+if (!buf)
+	return (0);
 rdlen = read(fd, buf, fsize);
 buf[fsize] = 0;
 if (rdlen <= 0)
 	return (free(buf), 0);
 close(fd);
 for (i = 0; i < fsize; i++)
-if (buf[i] == '/n')
-{
-buf[i] = 0;
-build_history_list(info, buf + last, linecount++);
-last = i + 1;
-}
+	if (buf[i] == '\n')
+	{
+		buf[i] = 0;
+		build_history_list(info, buf + last, linecount++);
+		last = i + 1;
+	}
 if (last != i)
 	build_history_list(info, buf + last, linecount++);
 free(buf);
