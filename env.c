6b43045700568233f571c98e2d9ffe9fe7b63fd3
ilyass <ilyass.daaif@gmail.com>
2023-08-23 11:38:14 -0700

    a

--- env.c
+++ env.c
@@ -2,7 +2,7 @@
 
 /**
  * _myenv - prints the current environment
- * @info: structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
@@ -36,24 +36,42 @@ return (NULL);
 
 /**
  * _mysetenv - Initialize a new environment variable,
- *             or modifi an existing one
- * @info: Structure containg potential arguments. Used to maintain
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
-int i;
-
-if (info->argc == 1)
+if (info->argc != 3)
 {
-_eputs("Too few arguements.\n");
+_eputs("Incorrect number of arguements\n");
 return (1);
 }
-for (i = 1; i <= info->argc; i++)
-	_unsetev(info, info->argv[i]);
+if (_setenv(info, info->argv[1], info->argv[2]))
+	return (0);
+return (1);
+}
 
-return (0);
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
 }
 
 /**
