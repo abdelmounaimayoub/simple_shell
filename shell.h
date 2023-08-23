c160f469d22aba1db350e158632d6b9d3e55a14a
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2023-08-23 17:55:25 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -27,11 +27,11 @@
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
-/* l if using system getline() */
+/* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
-#define HIST_FILE	".simlpe_shell_history"
+#define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
 
 extern char **environ;
